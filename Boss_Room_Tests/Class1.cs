b8bb3a67d2bb79c5b17ffd11a1b02801d2c60cbf
Shane Evans <shane@gamedriver.io>
2025-02-27 08:49:26 -0800

    Initial changes for GDC

--- Boss_Room_Tests/Class1.cs
+++ Boss_Room_Tests/Class1.cs
@@ -13,28 +13,30 @@ namespace DemoTest
     public class UnitTest
     {
 
-        private static string target = "IDE";
-        //private static string target = "standalone";
+        //private static string target = "IDE";
+        private static string target = "standalone";
 
-        //private static string host = "192.168.68.54";
+        //private static string host = "192.168.68.61";
         static string host = "localhost";
 
-        private static string platform = "desktop";
+        //private static string platform = "desktop";
         //private static string platform = "mobile";
 
         private static string gameMode = "offline";
         //public string gameMode = "online";
 
         private static string exePath = null;
-        //private static string exePath = "c:\Path\To\Game.exe";
+        //private static string exePath = @"c:\Path\To\Game.exe";
+        //private static string exePath = @"/Users/shaneevans/Desktop/BossRoom.app/Contents/MacOS/BossRoom";
 
         //These parameters can be used to override settings used to test when running from the NUnit command line
         public string testHost = TestContext.Parameters.Get("Host", host);
         public string testMode = TestContext.Parameters.Get("Mode", target);
         public string pathToExe = TestContext.Parameters.Get("pathToExe", exePath);
         public string lobbyMode = TestContext.Parameters.Get("lobby", gameMode);
+        public int testPort = TestContext.Parameters.Get("testPort", 19734);
 
-        string player = "//Player[@name='PlayerAvatar0']";
+        //string player = "//Player[@name='PlayerAvatar0']";
         string currentAnim;
 
         ApiClient api;
@@ -51,16 +53,16 @@ namespace DemoTest
                 if (pathToExe != null)
                 {
                     ApiClient.Launch(pathToExe);
-                    api.Connect(testHost, 19734, false, 30);
+                    api.Connect(testHost, testPort, false, 30);
                 }
 
                 // If no executable path was given, we will attempt to connect to the Unity editor and initiate Play mode
                 else if (testMode == "IDE")
                 {
-                    api.Connect(testHost, 19734, true, 30);
+                    api.Connect(testHost, testPort, true, 30);
                 }
                 // Otherwise, attempt to connect to an already playing game
-                else api.Connect(testHost);
+                else api.Connect(testHost, testPort, false, 60);
 
             }
             catch (Exception e)
@@ -94,7 +96,7 @@ namespace DemoTest
             api.Wait(2000);
             if (testMode == "IDE")
             {
-                api.ToggleEditorPlay();
+                api.StopEditorPlay();
             }
             api.Disconnect();
             api.Wait(2000);
---
+++ Boss_Room_Tests/run-tests.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/sh
+# This is intended to run the Boss_Room_Tests test using GameDriver
+
+mono ./nunit3-console.exe ../../../Boss_Room_Tests/bin/Debug/Boss_Room_Tests.dll --testparam:Mode=standalone --testparam:testHost=localhost
